9fc0fbac23582f9e14cb4b5472d2c191bad9b760
LearnToKode <mikecic3@gmail.com>
2021-02-03 17:29:21 +0100

    Commit example

--- app/src/main/java/com/example/myfirstgithub/MainActivity.kt
+++ app/src/main/java/com/example/myfirstgithub/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val blueButton: Button = findViewById(R.id.myButton)
         blueButton.setOnClickListener{
             // myy funcionality
