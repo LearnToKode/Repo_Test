e60c2b690fbed88cd2770734a79f5099baaf5ee0
LearnToKode <mikecic3@gmail.com>
2021-02-03 21:17:12 +0100

    New Message in new Button

--- app/src/main/java/com/example/myfirstgithub/MainActivity.kt
+++ app/src/main/java/com/example/myfirstgithub/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myfirstgithub
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +15,10 @@ class MainActivity : AppCompatActivity() {
         blueButton.setOnClickListener{
             // myy funcionality
 
+            Toast.makeText(this, "Esto es un mensaje", Toast.LENGTH_SHORT.show()
+
+
+            )
         }
     }
 
