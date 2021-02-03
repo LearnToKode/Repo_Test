46858fc253bc56297efaf5086205735b2c597b08
LearnToKode <mikecic3@gmail.com>
2021-02-03 17:21:14 +0100

    My button´s Funcionality
    - Listener added

--- app/src/main/java/com/example/myfirstgithub/MainActivity.kt
+++ app/src/main/java/com/example/myfirstgithub/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.myfirstgithub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val blueButton: Button = findViewById(R.id.myButton)
+        blueButton.setOnClickListener{
+            // myy funcionality
+
+        }
+    }
 
 
 
